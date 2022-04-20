138baaafcff9dfe4515209eee297377cd3d4d43f
gatsuBerserk <berserkdev@outlook.it>
2022-04-20 20:09:15 +0200

    style snack5

--- snack5/index.php
+++ snack5/index.php
@@ -20,12 +20,14 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <title>Snack 5</title>
 </head>
 <body>
     <?php 
-        var_dump($paragraph);
+        for($i = 0; $i < count($paragraph); $i++){
+            echo "<p>". $paragraph[$i] . "</p>";
+        }
     ?>
 </body>
 </html>
